59d1bb88234169739485a4811bdf6cb5611b39fd
David Bond <david.bond@panoramicdata.com>
2020-04-18 15:57:47 +0100

    Added unit tests

--- Cisco.DnaCenter.Test/DeviceTests.cs
+++ Cisco.DnaCenter.Test/DeviceTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using Cisco.DnaCenter.Api.Data;
+using FluentAssertions;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,6 +13,39 @@ namespace Cisco.DnaCenter.Test
 		}
 
 		[Fact]
+		public async void GetDeviceListAsync_Succeeds()
+		{
+			var devices = await GetDevices().ConfigureAwait(false);
+
+			var device = devices.Response[0];
+
+			device.Should().NotBeNull();
+			device.Id.Should().NotBeNull();
+			var deviceId = devices.Response[0].Id!;
+
+			// Get details for the first device
+			var deviceDetails = await Client
+				.Devices
+				.GetDeviceByIdAsync(deviceId)
+				.ConfigureAwait(false);
+			deviceDetails.Response.Should().NotBeNull();
+			deviceDetails.Response.SerialNumber.Should().NotBeNullOrEmpty();
+		}
+
+		private async Task<NetworkDeviceListResult> GetDevices()
+		{
+			var devices = await Client
+				.Devices
+				.GetDeviceListAsync()
+				.ConfigureAwait(false);
+
+			devices.Should().BeOfType<NetworkDeviceListResult>();
+			devices.Should().NotBeNull();
+			devices.Response.Should().NotBeNullOrEmpty();
+			return devices;
+		}
+
+		[Fact]
 		public async void GetAllInterfacesAsync_Succeeds()
 		{
 			var interfaces = await Client
@@ -18,8 +53,23 @@ namespace Cisco.DnaCenter.Test
 				.GetAllInterfacesAsync()
 				.ConfigureAwait(false);
 
+			interfaces.Should().BeOfType<DeviceIfListResult>();
 			interfaces.Should().NotBeNull();
 			interfaces.Response.Should().NotBeNullOrEmpty();
+		}
+
+		[Fact]
+		public async void GetDeviceCountAsync_Succeeds()
+		{
+			var deviceCount = await Client
+				.Devices
+				.GetDeviceCountAsync()
+				.ConfigureAwait(false);
+
+			deviceCount.Should().BeOfType<CountResult>();
+			deviceCount.Response.Should().NotBeNull();
+			deviceCount.Response.HasValue.Should().BeTrue();
+			deviceCount.Response.Value.Should().NotBe(0);
 		}
 	}
 }
