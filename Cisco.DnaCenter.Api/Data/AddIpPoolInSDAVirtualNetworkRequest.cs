a40f9d80fdcb772c4c24df8ab6a0e483af769450
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2020-05-12 16:59:39 +0100

    Revert "Delete AddIpPoolInSDAVirtualNetworkRequest.cs"
    This reverts commit 0896959c18ed913735bcb6a79d48913f0ccde801.

---
+++ Cisco.DnaCenter.Api/Data/AddIpPoolInSDAVirtualNetworkRequest.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Cisco.DnaCenter.Api.Data
+{
+	/// <summary>
+	/// AddIPPoolInSDAVirtualNetworkRequest
+	/// </summary>
+	[DataContract]
+	public class AddIpPoolInSDAVirtualNetworkRequest : List<AddIpPoolInSdaVirtualNetworkRequestInner>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AddIpPoolInSDAVirtualNetworkRequest" /> class.
+		/// </summary>
+		[JsonConstructor]
+		public AddIpPoolInSDAVirtualNetworkRequest()
+		{
+		}
+
+		/// <summary>
+		/// Returns the string presentation of the object
+		/// </summary>
+		/// <returns>String presentation of the object</returns>
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			sb.Append("class AddIPPoolInSDAVirtualNetworkRequest {\n");
+			sb.Append("}\n");
+			return sb.ToString();
+		}
+	}
+}
