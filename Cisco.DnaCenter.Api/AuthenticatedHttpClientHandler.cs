1352a8933873f674b21c1c7feef732d7a9f28343
Ryan Halley <ryan.halley@panoramic.systems>
2020-11-25 15:30:29 +0000

    Rerequest token if invalid.

--- Cisco.DnaCenter.Api/AuthenticatedHttpClientHandler.cs
+++ Cisco.DnaCenter.Api/AuthenticatedHttpClientHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
@@ -50,49 +51,68 @@ namespace Cisco.DnaCenter.Api
 			HttpRequestMessage request,
 			CancellationToken cancellationToken)
 		{
-			if (_token is null)
+			var retryCount = 0;
+			while (true)
 			{
-				// Token will be null when first connecting - this is fine.
-				// However, if the user is attempting a secure call, throw an exception
-				if (!request.RequestUri.AbsoluteUri.EndsWith("/dna/system/api/v1/auth/token"))
+				if (_token is null)
 				{
-					// We're not connected, so connect now (just in time)
-					await _dnaCenterClient
-						.ConnectAsync(cancellationToken)
-						.ConfigureAwait(false);
+					// Token will be null when first connecting - this is fine.
+					// However, if the user is attempting a secure call, throw an exception
+					if (!request.RequestUri.AbsoluteUri.EndsWith("/dna/system/api/v1/auth/token"))
+					{
+						// We're not connected, so connect now (just in time)
+						await _dnaCenterClient
+							.ConnectAsync(cancellationToken)
+							.ConfigureAwait(false);
+						request.Headers.Add("X-Auth-Token", _token);
+					}
+				}
+				else
+				{
 					request.Headers.Add("X-Auth-Token", _token);
 				}
-			}
-			else
-			{
-				request.Headers.Add("X-Auth-Token", _token);
-			}
 
-			// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
-			if (_logger.IsEnabled(_levelToLogAt))
-			{
-				_logger.Log(_levelToLogAt, $"Request\r\n{request}");
-				if (request.Content != null)
+				// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
+				if (_logger.IsEnabled(_levelToLogAt))
 				{
-					_logger.Log(_levelToLogAt, "RequestContent\r\n" + await request.Content.ReadAsStringAsync().ConfigureAwait(false));
+					_logger.Log(_levelToLogAt, $"Request\r\n{request}");
+					if (request.Content != null)
+					{
+						_logger.Log(_levelToLogAt, "RequestContent\r\n" + await request.Content.ReadAsStringAsync().ConfigureAwait(false));
+					}
 				}
-			}
 
-			var httpResponseMessage = await base
-				.SendAsync(request, cancellationToken)
-				.ConfigureAwait(false);
+				var httpResponseMessage = await base
+					.SendAsync(request, cancellationToken)
+					.ConfigureAwait(false);
 
-			// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
-			if (_logger.IsEnabled(_levelToLogAt))
-			{
-				_logger.Log(_levelToLogAt, $"Response\r\n{httpResponseMessage}");
-				if (httpResponseMessage.Content != null)
+				if (httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized
+					&& (await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false)).IndexOf("expired", StringComparison.InvariantCultureIgnoreCase) >= 0)
 				{
-					_logger.Log(_levelToLogAt, "ResponseContent\r\n" + await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false));
-				}
-			}
+					if (retryCount != 0)
+					{
+						// We've already tried this, so throw exception.
+						throw new InvalidOperationException("Unable to acquire new token.");
+					}
 
-			return httpResponseMessage;
+					// Invalidate the token so that on the next pass, a new token is requested.
+					_token = null;
+					retryCount++;
+					continue;
+				}
+
+				// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
+				if (_logger.IsEnabled(_levelToLogAt))
+				{
+					_logger.Log(_levelToLogAt, $"Response\r\n{httpResponseMessage}");
+					if (httpResponseMessage.Content != null)
+					{
+						_logger.Log(_levelToLogAt, "ResponseContent\r\n" + await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false));
+					}
+				}
+
+				return httpResponseMessage;
+			}
 		}
 	}
 }
